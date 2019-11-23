463c641a7caa3427e68f9e10ca94cd69ef905dbf
Dieter <vanderdt@ucmail.uc.edu>
2019-11-22 19:53:50 -0500

    ok. I got it now.

--- library/MyRoomba.py
+++ library/MyRoomba.py
@@ -1,42 +1,38 @@
 from pycreate2 import Create2
 from library import Ports
 
+
 class MyRoomba:
     def __init__(self, port=None):
         if port is None: port = Ports.get_port('FT231X')
         self.robot = Create2(port)
+        self.robot.start()
         self.robot.full()
 
     def set_motors(self, left_speed, right_speed):
+        if left_speed > 1: left_speed
+
         # in mm/s. The order in the api is different
         self.robot.drive_direct(right_speed, left_speed)
 
+    def set_display(self, text):
+        text = str(text)
+        text = text[0:4]
+        text = text.ljust(4,' ')
+        self.robot.digit_led_ascii(text)
+
     def get_sensors(self):
         sensors = self.robot.get_sensors()
+        sensors = sensors._asdict()
+        sensors = dict(sensors)
         return sensors
-
-
-
 
 
 if __name__ == "__main__":
     import time
     roomba = MyRoomba()
     roomba.set_motors(0,0)
-
-    opcode = 142
-    cmd = (100,)
-
-    sensor_pkt_len = 80
-
-    roomba.robot.SCI.write(opcode, cmd)
-    time.sleep(15)  # wait 15 msec
-    packet_byte_data = roomba.robot.SCI.read(sensor_pkt_len)
-    #sensors = SensorPacketDecoder(packet_byte_data)
-
-    #return sensors
-
-
-
+    roomba.set_display(123)
+    s = roomba.get_sensors()
 
 
