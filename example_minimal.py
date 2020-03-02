ad6e690874bc675a65b1f08e5a8d3aae2716a368
Dieter <vanderdt@ucmail.uc.edu>
2020-03-02 14:17:37 -0500

    added challenge example

--- example_minimal.py
+++ example_minimal.py
@@ -1,16 +1,4 @@
 from PD2030.roomba import Client
-import time
-#ip = '192.168.0.249'
 ip = 'WALL-E'
-c = Client.Client(name=ip, do_upload=True)
-
-
-c.start_remote_server()
-analog = c.get_adc()
-
-#for x in range(100):
-#    d = c.get_external_sensor('sonar1')
-#    print(d)
-#    time.sleep(0.25)
-
-#c.get_roomba_sensors()
+robot = Client.Client(name='WALL-E', do_upload=True)
+robot.start_remote_server()
