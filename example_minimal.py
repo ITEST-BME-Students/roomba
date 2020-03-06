af30b94c98f1fbc1a0f23a31fc98a75d0d1f6a2e
Dieter <vanderdt@ucmail.uc.edu>
2020-03-05 20:42:39 -0500

    After committing and pulling from windows
    Let's test this on linux.

--- example_minimal.py
+++ example_minimal.py
@@ -1,5 +1,8 @@
+
+
 from PD2030.roomba import Client
-robot = Client.Client(name='KITT', do_upload=False)
+
+robot = Client.Client(name='KITT', do_upload=True)
 robot.start_remote_server()
 robot.test_communication(message=['hello'])
 data = robot.get_adc()
