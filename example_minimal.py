04ce7c69655661e7c911f68cdf847603fbe3dbc7
Dieter <none>
2020-03-05 20:38:13 -0500

    simplified
    Now I understand what the issue is, I have simplified the solution. Also added some code that deletes old instances of the client.

--- example_minimal.py
+++ example_minimal.py
@@ -1,3 +1,6 @@
 from PD2030.roomba import Client
-robot = Client.Client(name='KITT', do_upload=True)
+robot = Client.Client(name='KITT', do_upload=False)
 robot.start_remote_server()
+robot.test_communication(message=['hello'])
+data = robot.get_adc()
+print(data)
